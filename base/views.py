f1147be0725613f238ee538fd9babb45e68b16cd
Snadeep Raj Saimon <sandeeprajsaimon999@gmail.com>
2022-05-29 17:28:16 +0000

    models

--- base/views.py
+++ base/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+from .models import *
+# rooms = [
+#     {
+#         'id':1,
+#         'name':'Lets learn Python'
+#     },
+#     {
+#         'id':2,
+#         'name':'Lets learn JS'
+#     },
+#     {
+#         'id':3,
+#         'name':'Lets learn Ruby'
+#     }
+# ]
 def home(request):
-    return render (request, 'home.html')
+    params = {}
+    rooms = Room.objects.all()
+    params["rooms"] = rooms
+    return render (request, 'base/home.html',params)
 
-def room(request):
+def room(request, pk):
+    room = None
+    params = {}
+    room = Room.objects.get(id = pk)
+    params["room"] = room
     # return HttpResponse("Room Page")
-    return render (request, 'room.html')
+    return render (request, 'base/room.html', params)
+
+def createRoom(request):
+    params = {}
+    return render (request, 'base/room_form.html', params)
